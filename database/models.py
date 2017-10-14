809fe61b7d70857da9dc6dae68c6884d8e8fa875
Alexjmsherman <alexjmsherman@gmail.com>
2017-10-14 00:12:02 -0400

    add database models for documents and sections

---
+++ database/models.py
@@ -0,0 +1,44 @@
+import configparser
+from sqlalchemy import Text, Column, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+
+
+Base = declarative_base()
+
+
+class Documents(Base):
+    __tablename__ = 'DOCUMENTS'
+
+    document_id = Column(Integer(), primary_key=True)
+    path = Column(Text())
+    document_text = Column(Text())
+    table_text = Column(Text())
+    last_modified_by = Column(Text())
+    author = Column(Text())
+    created = Column(Text())
+    last_printed = Column(Text())
+    revision = Column(Text())
+    num_tables = Column(Text())
+
+
+class Sections(Base):
+    __tablename__ = 'SECTIONS'
+
+    section_id = Column(Integer(), primary_key=True)
+    document_name = Column(Text())
+    section_name = Column(Text())
+    section_text = Column(Text())
+
+
+if __name__ == "__main__":
+    config = configparser.ConfigParser()
+    config.read('../config.ini')
+    DB_CONNECTION = config['USER']['DB_CONNECTION']
+
+    # create database tables
+    engine = create_engine(DB_CONNECTION)
+    Base.metadata.create_all(engine)
+
+
+
